aff32b2a262663309f77f40c67f3237c29fad840
Timon Claeys <timon.claeys2@student.odisee.be>
2023-06-06 08:54:44 +0200

    created highscores route

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { router as userRouter } from "./routers/users.js";
 import { router as gamesRouter } from "./routers/games.js"
+import { router as highscoreRouter } from "./routers/highscores.js"
 
 const app = express();
 app.use(express.json());
 
 app.use('/api/v1/users', userRouter);
-app.use('/api/v1/games', gamesRouter)
+app.use('/api/v1/games', gamesRouter);
+app.use('/api/v1/highscores', highscoreRouter);
 
 export { app }
